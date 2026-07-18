9bc5fa30a2141b0f2237b14819b195b164129f28
oscar_xlm <39261724+xiaoxlm@users.noreply.github.com>
2026-07-18 16:03:47 +0800

    first blood

---
+++ tests/easychat_test.go
@@ -0,0 +1,40 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/cloudwego/eino-ext/components/model/ark"
+	"github.com/cloudwego/eino/schema"
+	"github.com/joho/godotenv"
+)
+
+func TestEasyChat(t *testing.T) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	model, err := ark.NewChatModel(ctx, &ark.ChatModelConfig{
+		APIKey: os.Getenv("ARK_API_KEY"),
+		Model:  os.Getenv("ARK_MODEL"),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	input := []*schema.Message{
+		schema.SystemMessage("你是一个可爱的高中美少女"),
+		schema.UserMessage("你好呀"),
+	}
+
+	resp, err := model.Generate(ctx, input)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp.Content)
+}
